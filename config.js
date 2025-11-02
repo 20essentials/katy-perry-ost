fd075d8ed5ccbf00ce56a0e9f65940b949524801
DominicDev <s0l0estudi4r@gmail.com>
2025-11-02 14:08:35 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' KATY PERRY ';
+export const nameSongs = [
+	"LAST FRIDAY NIGHT", /*1*/
+	"HOT N COLD", /*2*/
+	"TEENAGE DREAM", /*3*/
+	"FIREWORK", /*4*/
+	"DARK HORSE ", /*5*/
+	"I KISSED A GIRL", /*6*/
+	"CALIFORNIA GURLS", /*7*/
+	"ROAR", /*8*/
+	"WIDE AWAKE ", /*9*/
+	"PART OF ME", /*10*/
+	"E.T. ", /*11*/
+	"THE ONE THAT GOT AWAY", /*12*/
+	"UNCONDITIONALLY", /*13*/
+	"NEVER REALLY OVER", /*14*/
+	"BON APPÉTIT", /*15*/
+	"SWISH SWISH", /*16*/
+	"CHAINED TO THE RHYTHM ", /*17*/
+	"WAKING UP IN VEGAS", /*18*/
+	"THIS IS HOW WE DO", /*19*/
+	"DAISEYS", /*20*/
+]
